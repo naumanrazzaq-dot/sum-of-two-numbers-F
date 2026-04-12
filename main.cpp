07c86612f4689e3b43e738de2faa0c2e97766ec5
Muhammad Nauman <maliknomigep@gmail.com>
2026-04-12 13:59:33 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int sum(int a, int b){
+int s=a+b;
+return s;
+                                // iss mein hum function mein bata degai k humein kya chaiye
+
+}
+
+int main()
+{
+ cout << sum(10, 9);
+    return 0;                       // iss mein hum just function k sath values bataiye gai or jitni dfa chahe print karwa le
+}
